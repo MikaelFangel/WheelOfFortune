b3f1a20f31b2c7707b549cfc974a64a3c1d87f10
mikael <mikael.fangel@pm.me>
2022-11-26 15:33:35 +0100

    fixed recompose bug and moved logic to viewmodel

--- app/src/main/java/com/dtu/wheeloffortune/GameScreenViewModel.kt
+++ app/src/main/java/com/dtu/wheeloffortune/GameScreenViewModel.kt
@@ -1,22 +1,25 @@
 package com.dtu.wheeloffortune
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.dtu.wheeloffortune.ui.GameScreenState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class GameScreenViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(GameScreenState())
     val uiState: StateFlow<GameScreenState> = _uiState.asStateFlow()
 
     init {
+        Log.d("Init", "The viewModel have been init")
         resetGame()
     }
 
     private fun initializeKeys() {
         for (c in 'a'..'z')
-            _uiState.value.newTest[c] = true
+            _uiState.value.isKeyGuessed[c] = true
     }
 
     private fun getRandomCategory(): String {
@@ -29,6 +32,27 @@ class GameScreenViewModel : ViewModel() {
 
     private fun getCurrentWordAsBlanks(word: String): String {
         return word.replace("[a-zA-Z]".toRegex(), " ")
+    }
+
+    fun keyPress(c: Char) {
+        _uiState.value.isKeyGuessed[c] = false
+
+        val indexes = getIndexesOfLetters(c)
+        if (indexes.isNotEmpty()) {
+            val tempString = StringBuilder(uiState.value.guessedWord)
+            for (i in indexes)
+                tempString[i] = c
+
+            _uiState.update { wordState -> wordState.copy(guessedWord = tempString.toString()) }
+        }
+    }
+
+    private fun getIndexesOfLetters(c: Char): List<Int> {
+        return uiState.value.currentWord
+            .lowercase()
+            .withIndex()
+            .filter { it.value == c }
+            .map { it.index }
     }
 
     private fun resetGame() {
