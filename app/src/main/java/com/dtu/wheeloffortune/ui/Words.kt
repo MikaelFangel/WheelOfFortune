55b8a5aa66a8bbb34b91511cc6f4c649b9319de4
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-11-27 15:35:09 +0100

    initial commit for random words and cats

---
+++ app/src/main/java/com/dtu/wheeloffortune/ui/Words.kt
@@ -0,0 +1,33 @@
+package com.dtu.wheeloffortune.ui
+
+val titleList = listOf(
+    "Frost",
+    "Aladdin"
+)
+
+val animalList = listOf(
+    "Dog",
+    "Rabbit",
+    "Cat",
+    "Elephant"
+)
+
+val placeList = listOf(
+    "Copenhagen",
+    "Bruxelles",
+    "Amsterdam",
+    "Odense",
+    "The Moon"
+)
+
+val nameList = listOf(
+    "Lars Von Trier"
+)
+
+val categories =
+    hashMapOf(
+        "title" to titleList,
+        "animal" to animalList,
+        "places" to placeList,
+        "name" to nameList
+    )
