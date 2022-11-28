064792c4796e0024c4423c8340d343b1dc73b053
mikael <mikael.fangel@pm.me>
2022-11-28 20:16:58 +0100

    moved wheel data to its own file

---
+++ app/src/main/java/com/dtu/wheeloffortune/data/WheelValues.kt
@@ -0,0 +1,19 @@
+package com.dtu.wheeloffortune.data
+
+val wheelValues = listOf(
+    3000,
+    1250,
+    800,
+    500,
+    500,
+    500,
+    500,
+    100,
+    800,
+    800,
+    1000,
+    1500,
+    600,
+    0,
+    0
+)
